98a0a3dae5fe239c5aa342c453c904cc3dce1d06
Ensar Veseli <ensarveseli015@gmail.com>
2022-08-04 21:28:58 +0200

    starting js

---
+++ 1.sessions/4.JavaScript/session-15/hello.js
@@ -0,0 +1,44 @@
+/*document.write('Hello World!')
+console.log('Hello World!')
+alert('Hello World!')*/
+//Koment vetem me nje rresht
+/*
+koment me shume rreshta
+*/
+/*
+//menyra e definimit te variablave
+let x = 10
+var y = 3
+
+//menyra e definimit te konstantave
+const PI = 3.14
+
+console.log(x)
+console.log(y)
+console.log(PI)
+
+console.log('----------------------')
+
+x = 20
+y= 7 + PI
+//PI = 4.2 - variablat const nuk mund ti ndryshosh me
+
+console.log(x)
+console.log(y)
+console.log(PI)
+
+*/
+/*
+{
+var x = 10//global scope - mundet mu per dor brenda dhe jashte bllokut
+let y = 12//block scope - mund te perdoret vetem ne block
+const z = 4//block scope - mund te perdoret vetem ne block
+console.log(y)
+}
+let y = 3
+console.log(y)
+
+{
+    console.log(y)
+}
+*/
